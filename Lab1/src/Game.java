c0c72af03e678839a162976d6178aa0da852bcf3
SEHERKHANPC\Seher Khan <seherk92@gmail.com>
2017-08-24 16:38:56 +0500

    Lab1 commit

---
+++ Lab1/src/Game.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Created by Seher Khan on 8/24/2017.
+ */
+public class Game {
+    public static void main(String[] args) throws IOException{
+        String str="In my Grandmother's trunk, I packed";
+        BufferedReader in= new BufferedReader(new InputStreamReader(System.in));
+        String move;
+
+
+        for(int i=1;i<5;i++){
+            System.out.print("Player "+i+", what did you pack? ");
+            move = in.readLine();
+            str+=", "+move;
+        }
+        System.out.print("Player 5, what did you pack? ");
+        move = in.readLine();
+        str+=" and "+move;
+        str+=".";
+
+        System.out.println("Result: "+str);
+        System.out.println("End of game.");
+    }
+
+
+}
---
+++ Lab1/src/PrintName.java
@@ -0,0 +1,39 @@
+/**
+ * Created by Seher Khan on 8/24/2017.
+ */
+//**********************************************************************
+// PrintName application
+// This application inputs a name and prints it in two different formats
+//**********************************************************************
+import java.io.*; // Package for stream readers
+public class PrintName
+{
+    public static void main(String[] args) throws IOException
+    {
+        String first; // Person’s first name
+        String last; // Person’s last name
+        String middle; // Person’s middle initial
+        String firstLast; // Name in first-last format
+        String lastFirst; // Name in last-first format
+        BufferedReader in; // Input stream for strings
+        in = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Enter first name: "); // Prompt for first name
+        first = in.readLine(); // Get first name
+        System.out.print("Enter last name: "); // Prompt for last name
+        last = in.readLine(); // Get last name
+        System.out.print("Enter middle initial: "); // Prompt for middle initial
+        middle = in.readLine(); // Get middle initial
+        firstLast = first + " " + last; // Generate first format
+        System.out.println("Name in first-last format is " + firstLast);
+        lastFirst = last + ", "+ first + ", "; // Generate second format
+        System.out.println("Name in last-first-initial format is " +
+                lastFirst + middle + ".");
+        String newFormat = first+" "+middle+". "+last;
+
+        System.out.println("Name in First-name Middle-initial. Last-name format is " +
+                newFormat);
+    }
+
+}
+
+
